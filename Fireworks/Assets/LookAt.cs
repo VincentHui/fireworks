9b55b7c81923e474fb25d3406900dd1c4b69e03b
Alex <Alex@Tim-Roths-Mac-Pro-2.local>
2014-10-20 19:21:33 +0100

    Combined stuff

---
+++ Fireworks/Assets/LookAt.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAt : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Ray r = new Ray (this.transform.position, transform.TransformDirection (Vector3.forward));
+
+		Debug.DrawRay (this.transform.position, transform.TransformDirection (Vector3.forward));
+		RaycastHit hit = new RaycastHit ();
+		if(Physics.Raycast(r, out hit, 20))
+		{
+			GameObject.Destroy(hit.transform.gameObject);
+		}
+	}
+}
---
+++ Fireworks/Assets/TimerSpawner.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimerSpawner : MonoBehaviour {
+	public GameObject lookable;
+	// Use this for initialization
+	void Start () {
+		timer = timerMax;
+
+	}
+	private float timer;
+	public float timerMax = 1;
+	// Update is called once per frame
+	void Update () {
+		timer -= Time.deltaTime;
+		if (timer <= 0) 
+		{
+			Vector3 t = Camera.main.transform.position + Random.onUnitSphere*10;
+			GameObject g = (GameObject)GameObject.Instantiate(lookable, t, Quaternion.LookRotation(-(t- Camera.main.transform.position)));
+			g.transform.parent = this.transform.parent;
+			timer = timerMax;
+		}
+	}
+}
