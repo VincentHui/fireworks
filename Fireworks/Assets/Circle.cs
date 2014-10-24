397afb6a7b9cf6fc4b5325a4494b4d9179355369
Alex <GinSoaked@users.noreply.github.com>
2014-10-24 23:10:48 +0100

    basic firework triggering

--- Fireworks/Assets/Circle.cs
+++ Fireworks/Assets/Circle.cs
@@ -1,25 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Circle : MonoBehaviour {
+public class Circle : MonoBehaviour
+{
 
-	// Use this for initialization
+    // Use this for initialization
     public Vector3 axis = new Vector3(0, 0, 1);
     public float timeStamp;
-    public float AmountOfTime = 1;   
-	void Start () {
+    public float AmountOfTime = 1;
+    void Start()
+    {
         timeStamp = Time.time;
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-        float angle = -((Time.time - timeStamp) / AmountOfTime) *360.0f;
-        transform.localRotation = Quaternion.AngleAxis(angle,axis);     
-        if (Time.time- timeStamp>(AmountOfTime))
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float angle = -((Time.time - timeStamp) / AmountOfTime) * 360.0f;
+        transform.localRotation = Quaternion.AngleAxis(angle, axis);
+        if (Time.time - timeStamp > (AmountOfTime))
         {
             transform.GetComponentInChildren<TrailRenderer>().time = 1.0f;
             transform.GetComponentInChildren<TrailRenderer>().materials[0].color = Color.red;
             this.enabled = false;
         }
-	}
+    }
 }
--- Fireworks/Assets/LookAt.cs
+++ Fireworks/Assets/LookAt.cs
@@ -1,22 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class LookAt : MonoBehaviour {
-
+    GameObject[] _fireworks;
 	// Use this for initialization
 	void Start () {
-
+        _fireworks = Resources.LoadAll<GameObject>("Fireworks");
+        Debug.Log(_fireworks.Length);
 	}
-	
+    
 	// Update is called once per frame
 	void Update () {
-		Ray r = new Ray (this.transform.position, transform.TransformDirection (Vector3.forward));
+		Ray r = new Ray (this.transform.position, transform.TransformDirection (Vector3.forward * 10));
 
 		Debug.DrawRay (this.transform.position, transform.TransformDirection (Vector3.forward));
 		RaycastHit hit = new RaycastHit ();
-		if(Physics.Raycast(r, out hit, 20))
+		if(Physics.Raycast(r, out hit, 50))
 		{
-			GameObject.Destroy(hit.transform.gameObject);
+            makeFirework(hit.transform.gameObject);
+            GameObject.Destroy(hit.transform.gameObject);
+           
 		}
 	}
+
+    void makeFirework(GameObject hit)
+    {
+        GameObject.Instantiate(_fireworks[Random.Range(0, _fireworks.Length)], new Vector3(hit.transform.position.x, 0, hit.transform.position.z), Quaternion.identity);
+        Debug.Log("made firework");
+    }
 }
