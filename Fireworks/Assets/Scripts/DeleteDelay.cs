eb3098c90ae0f25141230d6ef95c8ea288016f5f
Heather <h.macdougal@live.co.uk>
2015-10-08 15:21:57 +0100

    Delete fireworks and audio sources after delay. Thinned crowd.

---
+++ Fireworks/Assets/Scripts/DeleteDelay.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeleteDelay : MonoBehaviour
+{
+    public float delay;
+
+	// Use this for initialization
+	void Start () 
+    {
+        StartCoroutine(DeleteAfterDelay(delay));
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+
+    IEnumerator DeleteAfterDelay(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        Destroy(gameObject);
+    }
+}
