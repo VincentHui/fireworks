dd2bc804df66ff8cbb9450e87d527ceecb2e76f5
foeaxe <louis@visr-vr.com>
2015-10-09 16:42:19 +0100

    Goliathon is here

---
+++ Fireworks/Assets/SetDrawLayer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(MeshRenderer))]
+[ExecuteInEditMode]
+public class SetDrawLayer : MonoBehaviour {
+
+    public int Layer = 0;
+	void Update () {
+        GetComponent<MeshRenderer>().sortingOrder = Layer;
+	}
+}
